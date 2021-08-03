5c5f1d04bbf543f6af660aac990070dac430d892
LinusS1 <git@linus.sh>
2021-08-02 17:14:11 -0700

    Create base Preference Pane

---
+++ PrefPane/KeyPopperPrefPane.swift
@@ -0,0 +1,15 @@
+//
+//  PrefPane.swift
+//  KeyPopper PrefPane
+//
+//  Created by Linus Skucas on 8/2/21.
+//
+
+import Foundation
+import PreferencePanes
+
+class KeyPopperPrefPane: NSPreferencePane {
+    override func mainViewDidLoad() {
+        
+    }
+}
